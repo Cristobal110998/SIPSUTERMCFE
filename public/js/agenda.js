f5954b0364e94bd65900c49d14b0416db29c39e7
Cristobal110998 <cristobal.barajas@alumnos.udg.mx>
2021-09-23 23:01:41 -0500

    cambio_agendaJs

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,118 +1,90 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     let formulario = document.querySelector("#formularioEventos");
 
-    var calendarEl = document.getElementById('agenda');
+    var calendarEl = document.getElementById("agenda");
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      locale:"es",
-      displayEventTime:TextTrackCueList,
+        initialView: "dayGridMonth",
+        locale: "es",
+        displayEventTime: TextTrackCueList,
 
-      headerToolbar: {
-        left:'prev,next prevYear,nextYear today',
-        center:'title',
-        right:'dayGridMonth, dayGridWeek, timeGridDay',
-      },
+        headerToolbar: {
+            left: "prev,next prevYear,nextYear today",
+            center: "title",
+            right: "dayGridMonth, dayGridWeek, timeGridDay",
+        },
+        
+        //events: baseURL+"/evento/mostrar",
 
-      //events: baseURL+"/evento/mostrar",
+        eventSources: {
+            url: baseURL + "/evento/mostrar",
+            method: "POST",
+            extraParams: {
+                _token: formulario._token.value,
+            },
+        },
 
-      eventSources:{
-        url: baseURL+"/evento/mostrar",
-        method:"POST",
-        extraParams:{
-          _token: formulario._token.value,
-        }
-      },
+        dateClick: function (info) {
+            formulario.reset();
+            formulario.start.value = info.dateStr;
+            formulario.end.value = info.dateStr;
 
-      
-      dateClick:function(info){
-        formulario.reset();
-        formulario.start.value=info.dateStr;
-        formulario.end.value=info.dateStr;
+            $("#evento").modal("show");
+        },
 
-          $("#evento").modal("show");
-      },
-
-      eventClick:function(info){
-        var evento = info.event;
-        console.log(evento);
-        console.log(formulario.start.value);
-        console.log(evento.start);
-
-        //FALTO ESTO PARA TENER MISMO FORMATO DE FECHAS
-        var auxFormato = evento.start.toDateString("YY-m-d");
-        console.log(auxFormato);
-
-        if(formulario.start.value != evento.start){
-          axios.post(baseURL+"/evento/editar/"+info.event.id).
-          then(
-            (respuesta) => {
-              formulario.id.value=respuesta.data.id;
-              formulario.title.value=respuesta.data.title;
-              formulario.descripcion.value=respuesta.data.descripcion;
-              formulario.start.value=respuesta.data.start;
-              formulario.end.value=respuesta.data.end;
-              $("#evento").modal("show");
-            }
-            ).catch(
-              error=>{
-                if(error.response){
-                  console.log(error.response.data);
-                }
-              }
+        eventClick: function (info) {
+            var evento = info.event;
   
-            )
-        }else{
-            alert("Son iguales");
-        }
-
-      
-
-
-
-      }
+            axios
+                .post(baseURL + "/evento/editar/" + info.event.id)
+                .then((respuesta) => {
+                    formulario.id.value = respuesta.data.id;
+                    formulario.title.value = respuesta.data.title;
+                    formulario.descripcion.value = respuesta.data.descripcion;
+                    formulario.start.value = respuesta.data.start;
+                    formulario.end.value = respuesta.data.end;
+                    $("#evento").modal("show");
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        console.log(error.response.data);
+                    }
+                });
+        },
 
     });
-
-   
 
     calendar.render();
-    document.getElementById("btnGuardar").addEventListener("click",function(){
-        enviarDatos("/evento/agregar");
-        
+    document
+        .getElementById("btnGuardar")
+        .addEventListener("click", function () {
+            enviarDatos("/evento/agregar");
+        });
+    document
+        .getElementById("btnEliminar")
+        .addEventListener("click", function () {
+            enviarDatos("/evento/borrar/" + formulario.id.value);
+        });
+    document
+        .getElementById("btnModificar")
+        .addEventListener("click", function () {
+            enviarDatos("/evento/actualizar/" + formulario.id.value);
+        });
 
-    });
-    document.getElementById("btnEliminar").addEventListener("click",function(){
-      enviarDatos("/evento/borrar/"+formulario.id.value);
-    });
-    document.getElementById("btnModificar").addEventListener("click",function(){
-      enviarDatos("/evento/actualizar/"+formulario.id.value);
-    });
+    function enviarDatos(url) {
+        const datos = new FormData(formulario);
 
-    function enviarDatos(url){
+        const nuevaURL = baseURL + url;
 
-      const datos = new FormData(formulario);
-
-      const nuevaURL = baseURL+url;
-
-      axios.post(nuevaURL,datos).
-      then(
-        (respuesta) => {
-          calendar.refetchEvents();
-          $("#evento").modal("hide");
-        }
-        ).catch(
-          error=>{
-            if(error.response){
-              console.log(error.response.data);
-            }
-          }
-
-        )
-
+        axios
+            .post(nuevaURL, datos)
+            .then((respuesta) => {
+                calendar.refetchEvents();
+                $("#evento").modal("hide");
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log(error.response.data);
+                }
+            });
     }
-
-
-
-  });
+});
